4a3d251cdacd12cedcac87fccf360f1212b97425
Deimelia <jdmsg311@gmail.com>
2026-07-13 11:23:19 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/index.css",
+    "./index.html",
+    "./src/App.tsx}",
+    "./src/pages/CalendarPage.tsx}",
+    "./src/pages/CaseCreator.tsx}",
+    "./src/pages/CaseEditor.tsx}",
+    "./src/pages/CasesPage.tsx}",
+    "./src/pages/CaseViewer.tsx}",
+    "./src/pages/Dashboard.tsx}",
+    "./src/pages/LogInPage.tsx}",
+    "./src/pages/ReportingPage.tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
